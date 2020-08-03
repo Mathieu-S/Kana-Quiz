cc835ce49d6fff7c7b6c11420734d0dc01eb21d9
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-08-03 15:19:39 +0200

    EDIT: GenerateQuiz is base factory method

--- src/KanaQuiz.Core/Services/QuizFactory.cs
+++ src/KanaQuiz.Core/Services/QuizFactory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using KanaQuiz.Core.Models;
 using KanaQuiz.Core.Repositories;
@@ -25,16 +25,29 @@ namespace KanaQuiz.Core.Services
         /// <returns></returns>
         public Quiz CreateHiraganaQuiz(sbyte nbAnwsers = 4)
         {
+            return GenerateQuiz(nbAnwsers, KanaType.Katakana);
+        }
+        
+        /// <summary>
+        /// Build the quiz.
+        /// </summary>
+        /// <param name="nbAnwsers"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private Quiz GenerateQuiz(byte nbAnwsers, KanaType type)
+        {
+            IsCreatable(nbAnwsers, type);
+            
             var rng = new Random();
             var answers = new List<Kana>();
 
             // Get all the hiraganas
-            var hiraganas = (List<Kana>) _kanaRepository.GetAll();
+            var kanas = (List<Kana>) _kanaRepository.GetAll();
             
             // Add answers to quiz
             for (var i = 0; i < nbAnwsers; i++)
             {
-                answers.Add(hiraganas[rng.Next(0, hiraganas.Count)]);
+                answers.Add(kanas[rng.Next(0, kanas.Count)]);
             }
             
             // Selection a good answer randomly
@@ -44,7 +57,7 @@ namespace KanaQuiz.Core.Services
             var quiz = new Quiz()
             {
                 Title = "Guess this Hiragana",
-                Type = KanaType.Hiragana,
+                Type = type,
                 Answers = answers,
                 GoodAnswer = goodAnswer
             };

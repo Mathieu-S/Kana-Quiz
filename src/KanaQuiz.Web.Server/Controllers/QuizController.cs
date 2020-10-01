9985f7225d8a945e2a585d3cb1e86d4ddd46110b
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-10-01 11:52:52 +0200

    ADD*: QuizController

---
+++ src/KanaQuiz.Web.Server/Controllers/QuizController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using KanaQuiz.Core.Models;
+using KanaQuiz.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KanaQuiz.Web.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        [HttpGet]
+        public Quiz Get()
+        {
+            var goodAnwser = new Kana{ Id = 1, Type = KanaType.Hiragana, Romanji = "a", Value = "a"};
+
+            var anwsers = new List<Kana>
+            {
+                goodAnwser
+            };
+
+            return new Quiz {Title = "test", Type = KanaType.Hiragana, GoodAnswer = goodAnwser, Answers = anwsers};
+        }
+    }
+}

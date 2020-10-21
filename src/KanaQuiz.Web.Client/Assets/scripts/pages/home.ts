0634d454977f112e68b874600459e384baae06d4
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2020-10-21 17:40:05 +0200

    ADD*: router system

---
+++ src/KanaQuiz.Web.Client/Assets/scripts/pages/home.ts
@@ -0,0 +1,13 @@
+﻿import tippy from "tippy.js";
+import { Page } from "../utils";
+
+class HomePage implements Page {
+  Name = "Home";
+  public OnInit(): void {
+    tippy("#blop", {
+      content: "My tooltip!",
+    });
+  }
+}
+
+export const Home = new HomePage();
---
+++ src/KanaQuiz.Web.Client/Assets/scripts/utils/routeService.ts
@@ -0,0 +1,15 @@
+﻿import { Page } from "./page";
+
+export class RouteService {
+  public static ParseRoute(route: string, pages: Array<Page>): void {
+    let actualPage: Page;
+
+    if (route.length == 0) {
+      actualPage = pages.find((x) => x.Name == "Home");
+    } else {
+      actualPage = pages.find((x) => x.Name.toLowerCase() == route);
+    }
+
+    actualPage.OnInit();
+  }
+}
